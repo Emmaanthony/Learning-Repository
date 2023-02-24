57c9d52fda37d1d62c2bd29eabfa444e4266801c
Emmanuel Anthony <kcbusiness79@gmail.com>
2023-02-24 04:39:33 -0800

    update

---
+++ c-programming/Arithmetic_ex.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/*this code is used to check the number of egg*/
+int main()
+{
+	printf("The number of eggs for the day:\n");
+
+	int eggs;
+	eggs= 24;
+
+	printf("the number of eggs is %d eggs\n", eggs);
+
+	return 0;
+}
---
+++ c-programming/add_de.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/*
+* this code is used to calculate the addition numbers and decimals
+*/
+
+int main()
+{
+
+	double decimal;
+	int number;
+
+	printf("Please enter what you want to calculate: ");
+	scanf("%i + %f", &&number, decimal);
+
+	double result = number + decimal;
+
+	printf("the addition of %i number and %f decimal is %f\n", result);
+
+
+
+return 0;
+}
